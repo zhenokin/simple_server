b4a1660d4e0f7cdb3954ddc1a50c138dc7abde96
Yauheni Vaitkevich <zhenokin.lida@gmail.com>
2019-02-19 21:56:59 +0300

    some tests

--- routes/index.js
+++ routes/index.js
@@ -4,108 +4,119 @@ var router = express.Router();
 const News = require('../news/News');
 const Users = require('../users/Users');
 const { generateId } = require('../news/News.Helpers');
-const { 
-    setActiveUser,
-    canActiveUserDeleteNews,
-    canActiveUserEditNews 
+const {
+  setActiveUser,
+  canActiveUserDeleteNews,
+  canActiveUserEditNews
 } = require('../users/Users.Helpers');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.send('U must log in (POST request to /aut { name: ?, pass: ? }) or sign up (post request to /reg { name: ?, pass: ?, isAdmin: Boolean })');
+  res.send(
+    'U must log in (POST request to /aut { name: ?, pass: ? }) or sign up (post request to /reg { name: ?, pass: ?, isAdmin: Boolean })'
+  );
 });
 
-
 router.post('/aut', (req, res, next) => {
-    const { name, pass } = req.body;
-    Users.find({ name, pass }, (err, user) => {
+  const { name, pass } = req.body;
+  Users.find({ name, pass }, (err, user) => {
+    if (err) return console.error(err);
+    if (user.length) {
+      setActiveUser(user);
+      res.send('complete');
+    } else {
+      Users.find({ name }, (err, users) => {
         if (err) return console.error(err);
-        if (user.length) {
-            setActiveUser(user);
-            res.send('complete');
+        if (users.length) {
+          res.send('Pass or Name are not correct');
         } else {
-            Users.find({ name }, (err, users) => {
-                if (err) return console.error(err);
-                if (users.length) {
-                    res.send('Pass or Name are not correct'); 
-                } else {
-                    res.send('There isn\'n such user');
-                }
-            });
+          res.send("There isn'n such user");
         }
-    });
+      });
+    }
+  });
 });
 
 router.post('/reg', (req, res, next) => {
-    const { name, pass, isAdmin = false } = req.body;
-    const id = generateId();
-    const user = new Users({ id, name, pass, isAdmin });
-    user.save(err => {
-        if(err) return console.error(err);
-        setActiveUser(user);
-        res.send('access completed');
-    });
+  const { name, pass, isAdmin = false } = req.body;
+  const id = generateId();
+  console.log(req.body);
+  console.log(name, pass);
+  Users.find({ name }),
+    (err, user) => {
+      if (err) return console.log(err);
+      console.log('123123');
+      if (user.length) {
+        res.status(500).send('there is such users name');
+      } else {
+        const newUser = new Users({ id, name, pass, isAdmin });
+        newUser.save(err => {
+          if (err) return console.error(err);
+          setActiveUser(newUser);
+          res.send(JSON.stringify({ q: '123123' }));
+        });
+      }
+    };
 });
 
 router.use((req, res, next) => {
-    if (!req.body.activeUser) {
-        res.redirect('/');
-        res.end();
-    } else {
-        next();
-    }
+  if (!req.body.activeUser) {
+    res.redirect('/');
+    res.end();
+  } else {
+    next();
+  }
 });
 
 router.get('/news', (req, res, next) => {
-    News.find((err, news) => {
-        if (err) return console.error(err);
-        res.send(news);
-    });
+  News.find((err, news) => {
+    if (err) return console.error(err);
+    res.send(news);
+  });
 });
 
 router.get('/news/:id', (req, res) => {
-    News.find({ id: req.params.id },(err, news) => {
-        if (err) return console.error(err);
-        res.send(news);
-    });
+  News.find({ id: req.params.id }, (err, news) => {
+    if (err) return console.error(err);
+    res.send(news);
+  });
 });
 
-
 router.post('/news', (req, res) => {
-    if (canActiveUserEditNews()) {
-        const { title = '', description = '', id = generateId() } = req.body;
-        const newNews = new News({ id, title, description });
-    
-        newNews.save((err) => {
-            if (err) return console.error(err);
-            res.send('done');
-        });
-    } else {
-        res.send('U cann\'t edit news');
-    }
+  if (canActiveUserEditNews()) {
+    const { title = '', description = '', id = generateId() } = req.body;
+    const newNews = new News({ id, title, description });
+
+    newNews.save(err => {
+      if (err) return console.error(err);
+      res.send('done');
+    });
+  } else {
+    res.send("U cann't edit news");
+  }
 });
 
 router.put('/news/:id', (req, res) => {
-    const { title = '', description = '' } = req.body;
-    const { id } = req.params;
-    const newNews = new News({ id, title, description });
+  const { title = '', description = '' } = req.body;
+  const { id } = req.params;
+  const newNews = new News({ id, title, description });
 
-    newNews.save((err) => {
-        if (err) return console.error(err);
-        res.send('done');
-    });
+  newNews.save(err => {
+    if (err) return console.error(err);
+    res.send('done');
+  });
 });
 
 router.delete('/news/:id', (req, res) => {
-    if (canActiveUserDeleteNews()) {
-        const id = req.params.id;
-        News.findOneAndRemove({ id: id }, (err) => {
-            if (err) return console.error(err);
-            res.send('done');
-        });
-    } else {
-        res.send('U can\'n delete news');
-    }
+  if (canActiveUserDeleteNews()) {
+    const id = req.params.id;
+    News.findOneAndRemove({ id: id }, err => {
+      if (err) return console.error(err);
+      res.send('done');
+    });
+  } else {
+    res.send("U can'n delete news");
+  }
 });
 
 module.exports = router;
--- users/Users.js
+++ users/Users.js
@@ -8,12 +8,12 @@ const url = 'mongodb://localhost:27017';
 mongoose.connect(`${url}/${dbnName}`, { useNewUrlParser: true });
 
 const usersSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    pass: String,
-    isAdmin: Boolean
+  id: Number,
+  name: String,
+  pass: String,
+  isAdmin: Boolean
 });
 
-const News = mongoose.model('Users', usersSchema);
+const Users = mongoose.model('Users', usersSchema);
 
-module.exports = News;
+module.exports = Users;
